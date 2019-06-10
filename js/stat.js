aac0e966f96a8554d4cbbf526ee4f3286363386b
Наталья Беляева <kasyanka@bk.ru>
2019-06-10 18:58:58 +0300

    Сделаны цвета, выравненны по низу столбики, выведен текст

--- js/stat.js
+++ js/stat.js
@@ -26,17 +26,26 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)'); // тень
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'wheat'); // облако
 
-  ctx.font = '16px PT Mono'; // текст
-  ctx.fillText('Ура вы победили!', 110, 75);
-  ctx.fillText(' Список результатов:', 110, 90);
+
 
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, ' + Math.round(Math.random() * 255) + ', 1)';
+    }
+
     ctx.fillText(players[i], CLOUD_X + GAP + FONT_GAP + (GAP * 5 + BAR_WIDTH) * i, FONT_Y);
-    ctx.fillRect(CLOUD_X + GAP + (GAP * 5 + BAR_WIDTH) * i, GAP + GAP, BAR_WIDTH, (barHeight * times[i]) / maxTime);
+    ctx.fillRect(CLOUD_X + GAP + (GAP * 5 + BAR_WIDTH) * i, GAP + GAP + barHeight - barHeight * times[i] / maxTime, BAR_WIDTH, barHeight * times[i] / maxTime);
   }
+
+  ctx.fillStyle = 'black';
+  ctx.font = '16px PT Mono'; // текст
+  ctx.fillText('Ура вы победили!', 110, 250);
+  ctx.fillText(' Список результатов:', 110, 270);
 };
 
 
